47927caa23689e153fb67d702474d63f207bf565
Ensor, Charlie George (Capgemini) <charlie.ensor@capgemini.com>
2023-08-17 22:02:47 +0100

    updates

--- functions/logFunction.js
+++ functions/logFunction.js
@@ -4,4 +4,3 @@ exports = function(changeEvent) {
     console.log(`New document inserted with ID: ${fullDocument._id}`);
     // You can add more logic here, like sending notifications, processing the data, etc.
 };
-  
