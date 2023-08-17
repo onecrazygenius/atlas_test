6f1db3929eae236687c7f248b629716a6bcecae9
Ensor, Charlie George (Capgemini) <charlie.ensor@capgemini.com>
2023-08-17 22:07:31 +0100

    mimic staged alterations

--- functions/logFunction.js
+++ functions/logFunction.js
@@ -1,6 +1,6 @@
 exports = function(changeEvent) {
     const { fullDocument } = changeEvent;
     
-    console.log(`New document inserted with ID: ${fullDocument._id}`);
+    console.log(`New stage document inserted with ID: ${fullDocument._id}`);
     // You can add more logic here, like sending notifications, processing the data, etc.
 };
