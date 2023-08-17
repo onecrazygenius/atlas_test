7d12184a1cd6f2556f0afd3b5eec6564f61afe9f
Ensor, Charlie George (Capgemini) <charlie.ensor@capgemini.com>
2023-08-17 21:42:42 +0100

    basic function and trigger

---
+++ functions/logFunction.js
@@ -0,0 +1,7 @@
+exports = function(changeEvent) {
+    const { fullDocument } = changeEvent;
+    
+    console.log(`New document inserted with ID: ${fullDocument._id}`);
+    // You can add more logic here, like sending notifications, processing the data, etc.
+};
+  
