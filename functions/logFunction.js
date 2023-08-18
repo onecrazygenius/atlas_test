41bee4fd11179483e7bb7d8ff25e02e76a742360
Ensor, Charlie George (Capgemini) <charlie.ensor@capgemini.com>
2023-08-18 08:19:46 +0100

    feature updated

--- functions/logFunction.js
+++ functions/logFunction.js
@@ -1,6 +1,6 @@
 exports = function(changeEvent) {
     const { fullDocument } = changeEvent;
     
-    console.log(`New test document inserted with ID: ${fullDocument._id}`);
+    console.log(`New feature document inserted with ID: ${fullDocument._id}`);
     // You can add more logic here, like sending notifications, processing the data, etc.
 };
